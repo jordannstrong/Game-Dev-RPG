54cbe3042c83068f2fb84ba1122eb45d31b10c0d
BladeSniper95 <mossm7@students.rowan.edu>
2017-12-22 21:17:09 -0500

    Almost done

---
+++ Assets/Scripts/follow.cs
@@ -0,0 +1,40 @@
+﻿
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class follow : MonoBehaviour
+{
+ private GameObject player;
+ public float speed;
+ public Vector3 position;
+ private float distance;
+ //public Transform initialPosition;
+ public float moveSpeed;
+ public float visionRange;
+ public AnimationClip run;
+ public AnimationClip idle;
+	public static bool attack = false;
+
+ // Use this for initialization
+ void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+               //transform.position = initialPosition.position;
+        }
+ 	
+ // Update is called once per frame
+ void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+              if (distance < 5)
+        {
+            transform.position = transform.position;
+        }
+        else if (distance < visionRange)
+        {
+            Quaternion newRotation = Quaternion.LookRotation (position - transform.position);
+						newRotation.z = 0f;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+             }
+         }
+ }
